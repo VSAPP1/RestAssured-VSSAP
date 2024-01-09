d0ff02fc4a2e7e2c06827ffd1ded9948d00ad021
srinivas.raghupatruni <srinivas.raghupatruni@keylane.com>
2024-01-09 22:58:56 +0530

    Initial commit - project setup

---
+++ src/test/java/com/visitsg/api/utilities/globalConstants.java
@@ -0,0 +1,44 @@
+package com.visitsg.api.utilities;
+
+import com.github.javafaker.Faker;
+import com.visitsg.api.authentication.authenticationCommand;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
+
+public class globalConstants {
+
+    private static String configsPath = "src/test/resources/configs/configuration.properties";
+
+    public static Map getGlobalHeaders() {
+        return Map.of("Accept", "application/json", "content-type", "application/json");
+    }
+
+    public static Map getSessionHeaders() throws IOException {
+        return Map.of("UserSecurityToken", authenticationCommand.getAuthToken());
+    }
+
+    public static String getGlobalVariables(String key) throws java.io.IOException {
+        Properties properties = new Properties();
+        FileInputStream fileInputStream = new FileInputStream(configsPath);
+        properties.load(fileInputStream);
+        return properties.getProperty(key);
+    }
+
+    public static String getRandomName() {
+        Faker fakerNameTool = new Faker();
+        return fakerNameTool.superhero().name();
+    }
+
+    public static String getRandomEmail() {
+        Faker fakerNameTool = new Faker();
+        return fakerNameTool.artist().name() + "@keylane.com";
+    }
+
+    public static String getRandomContactPhoneNumber() {
+        Faker fakerNameTool = new Faker();
+        return fakerNameTool.phoneNumber().phoneNumber();
+    }
+}
